363a14b6cca9a86a0fc174034fe34ecfd173e81e
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-04 01:23:41 +0400

    Owncrypto-website

--- script.js
+++ script.js
@@ -53,3 +53,20 @@ const toggleActive = function () {
 }
 
 addEventOnElem(addToFavBtns, "click", toggleActive);
+
+/** scroll revreal effect */
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+          sections[i].classList.add("active");
+        } else {
+            sections[i].classList.remove("active");
+        }
+    }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
